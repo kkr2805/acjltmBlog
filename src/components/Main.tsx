568f6a2e5491007aceb1d8ae074594d53cfec7c3
ajcltm <ajcltm@gmail.com>
2022-08-30 00:37:16 +0900

    2022.8.30. 00:37

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">할아버지 자서전 속 흥미로운 사실들</h1>
+            <ul className="main--facts">
+                <li>할아버지가 가장 아끼는 형제는 만홍 동생이었다.</li>
+                <li>작은 어머니 제사를 부탁한 서울 사촌은 제사 비용 한푼 준적이 없다.</li>
+                <li>증조할아버지께서 돌아가셨을 때 할아버지의 직업은 경찰이었다.</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img className="nav--icon" src='/images/icon.jpg'/>
+            <h3 className="nav--logo_text">Ajcltm Blog</h3>
+            <h4 className="nav--title">할아버지 자서전</h4>
+        </nav>
+    )
+}
